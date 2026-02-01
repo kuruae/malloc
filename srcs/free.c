882b82270501b1f9a9f0311d8cf6662f69389ac8
kuruae <kurutsukipv@gmail.com>
2026-02-01 15:19:55 +0100

    ... brought free() back to O(n) for safety reasons

--- srcs/free.c
+++ srcs/free.c
@@ -1,6 +1,9 @@
 #include "alloc.h"
+#include "compiler_attrs.h"
+#include "libft.h"
 #include "zones.h"
 #include "chunk_utils.h"
+#include <stdio.h>
 
 t_zone_header *find_zone_for_ptr(const void *ptr, t_zone_type type) {
 	t_zone_header *zone;
@@ -12,18 +15,39 @@ t_zone_header *find_zone_for_ptr(const void *ptr, t_zone_type type) {
 		zone = g_zones.large;
 
 	while (zone) {
-		if (ptr >= (void *)zone && ptr < (void *)zone + zone->zone_size)
-			return zone;
-		zone = zone->next;
-	}
+    void *zone_end = (char *)zone + zone->zone_size;
+    void *first_valid_ptr = (char *)zone + sizeof(t_zone_header) + sizeof(t_chunk_header);
+
+    if (ptr >= first_valid_ptr && ptr < zone_end)
+        return zone;
+    zone = zone->next;
+}
 	return NULL;
 }
 
 void free(void *ptr) {
     if (UNLIKELY(!ptr))
-       return; 
+       return;
+
+    if (UNLIKELY((uintptr_t)ptr % ALIGNMENT) != 0) {
+        ft_putendl_fd("free(): misaligned pointer in free()", 2);
+        return;
+    }
+    
+    t_zone_header *zone = NULL;
+        if (!(zone = find_zone_for_ptr(ptr, ZONE_TINY)) &&
+            !(zone = find_zone_for_ptr(ptr, ZONE_SMALL)) &&
+            !(zone = find_zone_for_ptr(ptr, ZONE_LARGE))) {
+            return (void)ft_putendl_fd("free(): wild or foreign pointer", 2);
+        }
     
     t_chunk_header *chunk = get_chunk_from_ptr(ptr);
+
+    if (UNLIKELY(chunk->free == 1)) {
+        ft_putendl_fd("free(): double free detected", 2);
+        return;
+    
+    
     if (chunk->zone_type == ZONE_LARGE) {
         t_zone_header *zone = (t_zone_header *)((char *)chunk - sizeof(t_zone_header));
         remove_zone(zone);
