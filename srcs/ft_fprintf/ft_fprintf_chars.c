f13a69934f7f66f342a93266687080e54de03686
kuruae <kurutsukipv@gmail.com>
2026-02-10 22:03:24 +0100

    p

---
+++ srcs/ft_fprintf/ft_fprintf_chars.c
@@ -0,0 +1,19 @@
+#include "ft_fprintf.h"
+
+int fprint_char(int fd, int c) { return (write(fd, &c, 1)); }
+
+int fprint_string(int fd, char *s) {
+  int count;
+
+  count = 0;
+  if (!s) {
+    count = fprint_string(fd, "(null)");
+    return (count);
+  }
+  while (*s) {
+    fprint_char(fd, (int)*s);
+    s++;
+    count++;
+  }
+  return (count);
+}
